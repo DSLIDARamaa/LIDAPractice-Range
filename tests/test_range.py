f2bdc2d5b797befc130abf704c2c3f07eb4682d4
Ramaa Balaji <medrthi@leeds.ac.uk>
2023-10-03 15:57:08 +0100

    removed comment

--- tests/test_range.py
+++ tests/test_range.py
@@ -3,9 +3,3 @@ from src.range_function import ranges
 
 def test_point():
     assert ranges([[3,0] - [0,4.5]]) == [0,0]
-
-
-
-### scenario 1 point
-#    - Parameters:  [3,0 - 0,4.5]
-#    - Result: [0,0] "Coinciding number"
